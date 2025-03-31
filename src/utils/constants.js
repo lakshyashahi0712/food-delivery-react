a2c45a0d3591e1d68e9358faef05b54b450aec4b
Lakshya Shahi <lakshyashahi0712@gmail.com>
2025-03-31 18:50:34 +0530

    cors error solved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "https://static.vecteezy.com/system/resources/previews/015/776/570/original/restaurant-business-logo-illustration-vector.jpg"
 
-export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6565704&lng=77.3685477&restaurantId="
+export const MENU_URL = "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6565704&lng=77.3685477&restaurantId="
